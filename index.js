3650d9026d82fb0e4baee671409b6fce82c6f0fd
sozi <sozyarnadir@gmail.com>
2022-08-19 01:31:51 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("H1");
+document.body.appendChild(newHeader);
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Abram is the champion";
